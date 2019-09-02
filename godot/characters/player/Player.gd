0bdc8355b5936c5edc3747ef35081ee9ccfb4861
Callum <callum@sea-jay.net>
2019-09-02 11:47:20 +0100

    Update player direction

--- godot/characters/player/Player.gd
+++ godot/characters/player/Player.gd
@@ -15,6 +15,10 @@ onready var move_acceleration = move_speed / move_start_time
 onready var move_friction = move_speed / move_stop_time
 var move_velocity : Vector2
 
+var direction : Vector2
+
+export var reach = 4.0
+
 enum State {
 	Normal
 }
@@ -29,29 +33,42 @@ func _process(delta):
 	match(state):
 		State.Normal:
 			handle_movement(delta)
+			handle_direction()
 	
 	if debug.visible:
-		#debug_display_label_direction.text = str(move_direction)
+		debug_display_label_direction.text = str(direction)
 		debug_display_label_position.text = str(position)
 		debug_display_label_velocity.text = str(move_velocity)
 		debug_display_label_state.text = str(state)
 
 func handle_movement(delta):
+	var input_direction = Vector2.ZERO
+	
 	# Horizontal
 	if Input.is_action_pressed("move_left"):
 		move_velocity.x = Helpers.approach(move_velocity.x, -move_speed, move_acceleration * delta)
+		input_direction.x -= 1
 	elif Input.is_action_pressed("move_right"):
 		move_velocity.x = Helpers.approach(move_velocity.x, move_speed, move_acceleration * delta)
+		input_direction.x += 1
 	else:
 		move_velocity.x = Helpers.approach(move_velocity.x, 0.0, move_friction * delta)
 	
 	# Vertical
 	if Input.is_action_pressed("move_up"):
 		move_velocity.y = Helpers.approach(move_velocity.y, -move_speed, move_acceleration * delta)
+		input_direction.y -= 1
 	elif Input.is_action_pressed("move_down"):
 		move_velocity.y = Helpers.approach(move_velocity.y, move_speed, move_acceleration * delta)
+		input_direction.y += 1
 	else:
 		move_velocity.y = Helpers.approach(move_velocity.y, 0.0, move_friction * delta)
 	
 	# Apply the forces to the player
 	move_and_slide(move_velocity)
+	
+	if input_direction != Vector2.ZERO:
+		direction = input_direction
+
+func handle_direction():
+	pass
