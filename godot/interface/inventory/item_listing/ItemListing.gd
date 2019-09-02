862164f7642a6fc5c4474b5b396da89e0b40f9a9
Callum <callum@sea-jay.net>
2019-09-02 16:26:50 +0100

    Add cancel option to item commands list

--- godot/interface/inventory/item_listing/ItemListing.gd
+++ godot/interface/inventory/item_listing/ItemListing.gd
@@ -4,14 +4,29 @@ onready var items = Helpers.get_file_as_json("res://data/items.json")
 var item = "example"
 
 onready var item_preview : TextureRect = get_node("../../../../../VBoxContainer/ItemPreview")
-onready var item_command_panel : Panel = get_node("../../../../CommandPanel")
+onready var item_command_panel = get_node("../../../../CommandPanel")
 
 func _ready():
 	connect("button_down", self, "_on_Button_down")
+
+func _on_CancelButton_down():
+	if item_command_panel.visible:
+		item_command_panel.visible = false
 
 func _on_Button_down():
 	if not item_command_panel.visible:
 		item_command_panel.visible = true
 	
-		if items[item].has("icon"):
-			item_preview.texture = load(items[item]["icon"])
+	var command_box = item_command_panel.get_node("VBoxContainer")
+	
+	if command_box.get_child_count() > 0:
+		for command in command_box.get_children():
+			command.queue_free()
+
+	var button_cancel = Button.new()
+	button_cancel.text = "Cancel"
+	button_cancel.connect("button_down", self, "_on_CancelButton_down")
+	command_box.add_child(button_cancel)
+
+	if items[item].has("icon"):
+		item_preview.texture = load(items[item]["icon"])
