bc6a9b82f02b97d60fe2021bd5659646dac3ac32
Callum <callum@sea-jay.net>
2019-09-16 13:33:27 +0100

    I don't even know anymo

--- autoload/scene_changer/SceneChanger.gd
+++ autoload/scene_changer/SceneChanger.gd
@@ -1,9 +1,14 @@
 extends Node
 
-#onready var animation_player : AnimationPlayer = $AnimationPlayer
+onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 onready var map_root = get_tree().root.get_node("/root/Game/Maps")
 onready var maps = Helpers.get_file_as_json("res://data/maps.json")
+
+onready var map_current = map_root.get_child(0)
+
+func _ready():
+	print(map_current)
 
 func change_map(key, delay=0.0):
 	assert(maps.has(key))
@@ -12,7 +17,24 @@ func change_map(key, delay=0.0):
 	if delay > 0.0:
 		yield(get_tree().create_timer(delay).start, "timeout")
 	
+	save_map(key)
 	call_deferred("_deferred_map_change", maps[key])
+
+func save_map(key):
+	var file = File.new()
+	var file_path = "user://maps.json"
+	file.open(file_path, File.WRITE)
+	
+	var nodes = [ map_current ]
+	var save_data = []
+	
+	while not nodes.empty():
+		var node = nodes.pop_front()
+		nodes += node.get_children()
+	
+	print(to_json(save_data))
+	
+	file.close()
 
 func _deferred_map_change(path):
 	if map_root.get_children().size() > 0:
@@ -23,3 +45,4 @@ func _deferred_map_change(path):
 	var map = load(path)
 	var instance = map.instance()
 	map_root.add_child(instance)
+	map_current = instance
