b2937f4b248e459ef75c72489581588019a0e603
Callum <callum@sea-jay.net>
2019-10-20 18:47:38 +0100

    Add text scrolling

--- interface/dialogue_box/DialogueBox.gd
+++ interface/dialogue_box/DialogueBox.gd
@@ -1,13 +1,30 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var scroll_speed = 0.1
 
-# Called when the node enters the scene tree for the first time.
+onready var label_speaker : Label = $HBoxContainer/VBoxContainer/HBoxContainer/Label
+onready var label_message : Label = $HBoxContainer/VBoxContainer/PanelContainer/VBoxContainer/Label
+var display_text : String
+var cursor_position = 0
+
+signal text_displayed
+
 func _ready():
-	pass # Replace with function body.
+	display_text = label_message.text
+	
+	scroll_text(scroll_speed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	label_message.text = display_text.substr(0, cursor_position)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		cursor_position = display_text.length()
+
+func scroll_text(speed):
+	cursor_position += 1
+	yield(get_tree().create_timer(scroll_speed), "timeout")
+	
+	if cursor_position < display_text.length():
+		scroll_text(speed)
+	else:
+		emit_signal("text_displayed")
